e24860ce52c04b56824db59a71139474c183b4b8
Ismael Carrillo <iscarrillo65@gmail.com>
2023-10-04 16:06:56 -0300

    commit 4.0  Ej Array_Matrices, y Array_String

--- Arrays_Matrices/Ejercicio_11.java
+++ Arrays_Matrices/Ejercicio_11.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Ejercicio_11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int[][] matriz_1 = {
                 {1,2,3,4},
                 {5,6,7,8}};
+
         for (int i = 0; i < matriz_1.length ; i++) {
             for (int j = 0; j < matriz_1[0].length; j++) {
                 System.out.print(matriz_1[i][j]+" ");
             }
             System.out.println(" ");
         }
+
         int[][] matriz_t = transponer_matriz(matriz_1);
         System.out.println("Mostrando matriz transpuesta  ");
+
         for (int i = 0; i < matriz_t.length ; i++) {
             for (int j = 0; j < matriz_t[0].length; j++) {
                 System.out.print(matriz_t[i][j]+" ");
